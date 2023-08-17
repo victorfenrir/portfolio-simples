b513689239c54637c22d77bdf82a9afca48a2634
Victor Barra <victorbarrapereira@gmail.com>
2023-08-17 17:13:08 -0300

    v1.1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import ListaDeRepositorios from './ListaRepositorios/index.js'
 
 function App() {
   return (
-    <ListaDeRepositorios />
+    <><>
+      <h1>Portfolio de Victor Barra</h1>
+      <h2>Desenvolvedor Front-end Jr.</h2>
+    </>
+    
+    <ListaDeRepositorios /></>
   );
 }
 
